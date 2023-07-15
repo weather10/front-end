89547c338149fff3507ff24d27f73ce85bedb142
daaraam <usb_adapter@naver.com>
2023-07-16 04:11:53 +0900

    feat: sign 마크업 완료

--- src/ootdproject/pages/SignIn.jsx
+++ src/ootdproject/pages/SignIn.jsx
@@ -9,60 +9,96 @@ function SignIn() {
 	const onSubmitHandler = event => {
 		event.preventDefault();
 		if (id.trim() === '' && pw.trim() === '') {
-			alert('ID, PW를 입력해주세요.');
+			alert('ID, PW를 입력하세요.');
+		} else if (!id) {
+			alert('email 혹은 ID를 입력하세요.');
+		} else if (!pw) {
+			alert('Password를 입력하세요.');
 		}
 	};
 
 	return (
-		<StSignInContainer>
+		<StOotdGramContainer>
 			<StSignLogo src={signLogo} alt="로고" />
 
 			<StInputForm onSubmit={onSubmitHandler}>
-				<StSignInput value={id} onChange={e => setId(e.target.value)} type="text" />
-				<StSignInput value={pw} onChange={e => setPw(e.target.value)} type="password" />
-				<StSignButton>Sign in</StSignButton>
+				<StSignInput
+					placeholder="email or username"
+					value={id}
+					onChange={e => setId(e.target.value)}
+					type="text"
+				/>
+				<StSignInput placeholder="PassWord" value={pw} onChange={e => setPw(e.target.value)} type="password" />
+				<StSignButton type="submit" $bgColor={'blue'}>
+					Sign in
+				</StSignButton>
 			</StInputForm>
-		</StSignInContainer>
+			<p style={{ color: '#dfdbdb' }}>――――――――　OR　――――――――</p>
+			<StSignButton $bgColor={'gray'}>Sign up</StSignButton>
+		</StOotdGramContainer>
 	);
 }
 
 export default SignIn;
 
-const StSignLogo = styled.img`
+export const StSignLogo = styled.img`
 	width: 200px;
 	height: 50px;
+	margin-bottom: 80px;
 `;
 
-const StSignInContainer = styled.div`
+export const StOotdGramContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	justify-content: center;
 	align-items: center;
 	width: 60%;
-	height: 120vh;
-	background-color: pink;
+	height: 100vh;
+	background-color: #fafafa;
 `;
 
-const StInputForm = styled.form`
+export const StInputForm = styled.form`
 	display: flex;
 	flex-direction: column;
+	justify-content: center;
 	align-items: center;
+	margin-bottom: 20px;
 `;
 
-const StSignInput = styled.input`
-	background-color: #f4f4f4;
+export const StSignInput = styled.input`
+	background-color: #dfdbdb;
+	width: 320px;
+	height: 20px;
+	border: none;
+	border-radius: 6px;
+	margin: 8px;
+	padding: 10px;
+`;
+
+export const StSignButton = styled.button`
 	width: 320px;
 	height: 40px;
 	border: none;
 	border-radius: 6px;
-	margin: 4px;
-`;
-
-const StSignButton = styled.button`
-	background-color: rgba(72, 132, 238, 0.6);
-	width: 320px;
-	height: 40px;
-	border: none;
-	border-radius: 6px;
-	margin: 4px;
+	margin: 8px;
+	padding: 10px;
+	margin-top: 32px;
 	cursor: pointer;
+	&:active {
+		filter: brightness(70%);
+	}
+	${props => colorHandler(props.$bgColor)};
+
+	${({ $bgColor }) => colorHandler($bgColor)};
 `;
+
+const colorHandler = color => {
+	switch (color) {
+		case 'blue':
+			return `background-color:rgba(72, 132, 238, 0.6); color : white;`;
+		case 'gray':
+			return `color: black; background-color: #dfdbdb`;
+		default:
+			return '';
+	}
+};
--- src/ootdproject/pages/SignUp.jsx
+++ src/ootdproject/pages/SignUp.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from 'react';
+import signLogo from '../icon/logo.png';
+import { StInputForm, StSignButton, StOotdGramContainer, StSignInput, StSignLogo } from './SignIn';
 
-function signUp() {
-	return <div>signup</div>;
+function SignUp() {
+	const [id, setId] = useState('');
+	const [pw, setPw] = useState('');
+	const [pw2, setPw2] = useState('');
+	const [email, setEmail] = useState('');
+
+	const onSubmitHandler = event => {
+		event.preventDefault();
+		if (id.trim() === '' && pw.trim() === '') {
+			alert('ID를 입력해주세요.');
+		} else if (!email) {
+			alert('email을 입력해주세요.');
+		} else if (!id) {
+			alert('ID를 입력해주세요.');
+		} else if (!pw) {
+			alert('password를 입력해주세요.');
+		} else {
+			alert('비밀번호 확인을 입력해주세요.');
+		}
+	};
+
+	return (
+		<StOotdGramContainer>
+			<StSignLogo src={signLogo} alt="로고" />
+
+			<StInputForm onSubmit={onSubmitHandler}>
+				<StSignInput placeholder="User Name" value={id} onChange={e => setId(e.target.value)} type="text" />
+				<StSignInput placeholder="e-mail" value={email} onChange={e => setEmail(e.target.value)} type="text" />
+				<StSignInput placeholder="Password" value={pw} onChange={e => setPw(e.target.value)} type="password" />
+				<StSignInput
+					placeholder="Password double check"
+					value={pw2}
+					onChange={e => setPw2(e.target.value)}
+					type="password"
+				/>
+				<StSignButton type="submit" $bgColor={'blue'}>
+					Sign up
+				</StSignButton>
+			</StInputForm>
+			<p style={{ color: '#dfdbdb' }}>――――――――　OR　――――――――</p>
+			<StSignButton $bgColor={'gray'}>
+				Already have an account? &nbsp;{' '}
+				<span
+					style={{
+						fontWeight: '700',
+					}}
+				>
+					Sign in.
+				</span>
+			</StSignButton>
+		</StOotdGramContainer>
+	);
 }
 
-export default signUp;
+export default SignUp;
