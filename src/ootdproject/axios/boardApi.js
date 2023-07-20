9bb6c65805383032f1490ff9ec0f02f7b2972af4
hotcream3904 <130561236+hotcream3904@users.noreply.github.com>
2023-07-20 10:44:42 +0900

    fix: 댓글api 오류수정중

--- src/ootdproject/axios/boardApi.js
+++ src/ootdproject/axios/boardApi.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 // 게시글 조회
 export const getBoard = async () => {
 	try {
 		const response = await axios.get(`${process.env.REACT_APP_SERVER}/api/posts`);
-		console.log('게시글 조회 data', response.data);
+		console.log("게시글 조회 data", response.data);
 		return response.data;
 	} catch (error) {
-		console.error('게시글조회에러', error);
+		console.error("게시글조회에러", error);
 		throw error;
 	}
 };
@@ -16,16 +16,16 @@ export const getBoard = async () => {
 export const postBoard = async (data, headers) => {
 	try {
 		const response = await axios.post(`${process.env.REACT_APP_SERVER}/api/post`, data, { headers });
-		console.log('boarderApi - response.data', response.data); // 성공
+		console.log("게시글 작성완료", response.data); // 성공
 		return response.data;
 	} catch (error) {
-		console.error('게시글 작성하기 에러', error);
+		console.error("게시글 작성하기 에러", error);
 		throw error;
 	}
 };
 
 // 게시글 좋아요 POST
-export const postLike = async payload => {
+export const postLike = async (payload) => {
 	try {
 		const response = await axios.post(`${process.env.REACT_APP_SERVER}/api/post/{postId}/like`, payload);
 		return response.data;
@@ -35,7 +35,7 @@ export const postLike = async payload => {
 };
 
 // 게시글 좋아요 취소 PUT
-export const putLikeCancel = async payload => {
+export const putLikeCancel = async (payload) => {
 	try {
 		const response = await axios.put(`${process.env.REACT_APP_SERVER}/api/post/{postId}/like`, payload);
 		return response.data;
--- src/ootdproject/components/Modal/Comments.jsx
+++ src/ootdproject/components/Modal/Comments.jsx
@@ -4,18 +4,17 @@ import yellowGirl from "../../icon/yellowGirl.jpg";
 import { GrClose } from "react-icons/gr";
 import Avatar from "../home/Avatar";
 import basic from "../../icon/basicAvatar.png";
-import { getComments } from "../../axios/comments";
+import { getComments, postComments, deleteComments, patchComments } from "../../axios/commentsApi";
 
 //openComments는 OotdCard에 있는 댓글아이콘(버튼)을 클릭했을때 state의 변한 값임!
 //toggleCommentsHandler는 Ootd에서 실행되는 함수인데 여기서 온클릭을 했을때 바깥에 있는 함수에까지 전달되도록 props를 받은것!
 function Comments({ openComments, toggleCommentsHandler, id }) {
-	const nickname = "eunji__2f";
-	const textmsg = "ㄴㅇ럼니ㅏ어림나ㅓ림너리ㅏㅁㄴ어라ㅣㄴ머하ㅣㄴ멍ㅎ;멓ㄴ;함ㄴㅇㅎ";
-	const dateTimeString = "2023-07-16T03:32:51.078944";
-	const result = dateTimeString.replace(/T.*/, "");
+	// const result = dateTimeString.replace(/T.*/, "");
 
+	//댓글조회 data
 	const [data, setData] = useState([]);
 
+	//댓글 조회 GET Api
 	const fetchComments = async () => {
 		try {
 			const data = await getComments(id);
@@ -29,53 +28,149 @@ function Comments({ openComments, toggleCommentsHandler, id }) {
 		fetchComments();
 	}, []);
 
-	const onSubmitHandler = (event) => {
-		event.preventDefault(); //있으면 좋은건가용? - 폼태그 액션
+	const [content, setContent] = useState("");
+
+	const AddCommentsHandler = (e) => {
+		setContent(e.target.value);
 	};
 
-	return (
-		<div>
-			{openComments && (
-				<>
-					<StModalsFather>
-						<StModalOneChild>
-							<StImgBox>
-								<StImg src={yellowGirl} />
-							</StImgBox>
-							<StCommentsBox>
-								<StButtonBox>
-									<GrClose onClick={toggleCommentsHandler} />
-								</StButtonBox>
-								<StGetCommentsBox>
-									{data.map((item) => (
-										<UserComment>
-											<Avatar image={item.userImage} type='homeAvatar' />
-											<StTextBox>
-												<StIdText>{item.nickname}</StIdText>
-												<StDate>{item.createdAt}</StDate>
-											</StTextBox>
-											<StUserEditCancelBtnBox>
-												<StUserBtn>수정</StUserBtn>
-												<StUserBtn>삭제</StUserBtn>
-											</StUserEditCancelBtnBox>
-										</UserComment>
-									))}
-								</StGetCommentsBox>
-								<StInputBox>
-									<form>
-										<StInput placeholder='댓글 달기...' />
-										<StAddComments onClick={onSubmitHandler}>게시</StAddComments>
-									</form>
-								</StInputBox>
-							</StCommentsBox>
-						</StModalOneChild>
-					</StModalsFather>
-				</>
-			)}
-		</div>
-	);
-}
+	//댓글 등록 POST Api
+	const addComments = async () => {
+		try {
+			const token = document.cookie.replace(/(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/, "$1");
+			const headers = {
+				Accept: "*/*",
+				Authorization: `${token}`,
+				"Content-Type": "application/json",
+			};
 
+			const payload = {
+				content,
+			};
+			const response = await postComments(id, headers, payload);
+			console.log("comment resData", response);
+		} catch (error) {
+			console.error("댓글 등록 실패:", error);
+		}
+	};
+
+	//댓글 삭제 DELETE api
+	const removeComments = async () => {
+		try {
+			const token = document.cookie.replace(/(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/, "$1");
+			const headers = {
+				Accept: "*/*",
+				Authorization: `${token}`,
+				"Content-Type": "application/json",
+			};
+
+			const response = await deleteComments(id, headers);
+			console.log("comment resData", response);
+		} catch (error) {
+			console.error("댓글 등록 실패:", error);
+		}
+	};
+
+	const onSubmitHandler = (event) => {
+		event.preventDefault(); //있으면 좋은건가용? - 폼태그 액션
+		addComments();
+
+		document.location.reload(true);
+	};
+
+	//댓글 수정상태 true
+	const [isOpen, setIsOpen] = useState(false);
+
+	const openUpdateHandler = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const onChangeUpdate = () => {};
+
+	//댓글 수정 PATCH api
+	const updateComments = async () => {
+		try {
+			const token = document.cookie.replace(/(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/, "$1");
+			const headers = {
+				Accept: "*/*",
+				Authorization: `${token}`,
+				"Content-Type": "application/json",
+			};
+
+			const response = await patchComments(id, headers);
+			console.log("수정성공", response);
+		} catch (error) {
+			console.error("댓글 수정 실패:", error);
+		}
+
+		return (
+			<div>
+				{openComments && (
+					<>
+						<StModalsFather>
+							<StModalOneChild>
+								<StImgBox>
+									<StImg src={yellowGirl} />
+								</StImgBox>
+								<StCommentsBox>
+									<StButtonBox>
+										<GrClose onClick={toggleCommentsHandler} />
+									</StButtonBox>
+									<StGetCommentsBox>
+										{data.map((item) => (
+											<UserComment>
+												<Avatar image={item.userImage} type='homeAvatar' />
+												{isOpen ? (
+													<StTextBox>
+														<StIdText>
+															{item.nickname}
+															<Update
+																type='text'
+																onChange={onChangeUpdate}
+																defaultValue={item.content}
+															/>
+														</StIdText>
+														<StDate>{item.createdAt}</StDate>
+													</StTextBox>
+												) : (
+													<StTextBox>
+														<StIdText>
+															{item.nickname}
+															{item.content}
+														</StIdText>
+														<StDate>{item.createdAt}</StDate>
+													</StTextBox>
+												)}
+												<StUserEditCancelBtnBox>
+													{isOpen ? (
+														<StUserBtn onClick={updateComments}>완료</StUserBtn>
+													) : (
+														<StUserBtn onClick={openUpdateHandler}>수정</StUserBtn>
+													)}
+													<StUserBtn onClick={removeComments}>삭제</StUserBtn>
+												</StUserEditCancelBtnBox>
+											</UserComment>
+										))}
+									</StGetCommentsBox>
+									<StInputBox>
+										<form onSubmit={AddCommentsHandler}>
+											<StInput
+												placeholder='댓글 달기...'
+												value={content}
+												onChange={AddCommentsHandler}
+											/>
+											<StAddComments onClick={onSubmitHandler}>게시</StAddComments>
+										</form>
+									</StInputBox>
+								</StCommentsBox>
+							</StModalOneChild>
+						</StModalsFather>
+					</>
+				)}
+			</div>
+		);
+	};
+}
 export default Comments;
 
 const StModalsFather = styled.div`
@@ -211,3 +306,5 @@ const StUserBtn = styled.button`
 	}
 	font-weight: 550;
 `;
+
+const Update = styled.input``;
